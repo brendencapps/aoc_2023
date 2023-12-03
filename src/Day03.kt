97ccd415517e058ae4b2c3b6a81aedc2b9dabb37
Brenden Capps <brenden.capps@gmail.com>
2023-12-03 00:21:08 -0600

    Day 3

---
+++ src/Day03.kt
@@ -0,0 +1,73 @@
+import java.io.File
+
+fun main() {
+    checkSolution("Day 3 Part 1 Example", day3Part1Solution("inputs/day03/example.txt"), 4361)
+    checkSolution("Day 3 Part 1", day3Part1Solution("inputs/day03/part1.txt"), 554003)
+    checkSolution("Day 3 Part 2 Example", day3Part2Solution("inputs/day03/example.txt"), 467835)
+    checkSolution("Day 3 Part 2", day3Part2Solution("inputs/day03/part1.txt"), 87263515)
+}
+
+data class Symbol(val symbol: Char, val row: Int, val col: Int) {
+    fun nextToPartNumber(part: PartNumber): Boolean {
+        return (col in part.col - part.number.length - 1..part.col) && (row in part.row - 1..part.row + 1)
+    }
+}
+data class PartNumber(val number: String, val row: Int, val col: Int )
+
+data class ProblemInput(val path: String, val numberList: MutableList<PartNumber> = mutableListOf(), val symbols: MutableList<Symbol> = mutableListOf()) {
+    init {
+        File(path).readLines().forEachIndexed { row, value ->
+            var currentNumber: String? = null
+            value.forEachIndexed { col, ch ->
+                if (ch.isDigit()) {
+                    if (currentNumber == null) {
+                        currentNumber = ""
+                    }
+                    currentNumber += ch
+                } else {
+                    if (ch != '.') {
+                        symbols.add(Symbol(ch, row, col))
+                    }
+                    if (currentNumber != null) {
+                        numberList.add(PartNumber(currentNumber!!, row, col))
+                        currentNumber = null
+                    }
+                }
+            }
+            if (currentNumber != null) {
+                numberList.add(PartNumber(currentNumber!!, row, value.length))
+                currentNumber = null
+            }
+        }
+    }
+}
+
+fun day3Part1Solution(path: String): Int {
+    val problemInput = ProblemInput(path)
+    return problemInput.numberList.sumOf { number ->
+        if(problemInput.symbols.any { it.nextToPartNumber(number) }) {
+            number.number.toInt()
+        }
+        else {
+            0
+        }
+    }
+}
+
+fun day3Part2Solution(path: String): Int {
+    val problemInput = ProblemInput(path)
+    return problemInput.symbols.sumOf { symbol ->
+        if(symbol.symbol == '*') {
+            val adjacentNumbers = problemInput.numberList.filter { symbol.nextToPartNumber(it) }
+            if(adjacentNumbers.size == 2) {
+                adjacentNumbers[0].number.toInt() * adjacentNumbers[1].number.toInt()
+            }
+            else {
+                0
+            }
+        }
+        else {
+            0
+        }
+    }
+}
